c458d3149453b026ca6014601db7ab6cceffdb89
davityeghoyan <dav1987dav>
2021-06-25 21:56:03 +0400

    update playfab utility version to 2.0.0

--- AutoGenerate/GenerateEnumsFiles.cs
+++ AutoGenerate/GenerateEnumsFiles.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using PlayFab.AdminModels;
 using UnityEditor;
-using UnityEngine;
 
 namespace PlayFabUtilityEditor.GenerateEnumsFiles
 {
@@ -37,6 +38,7 @@ namespace PlayFabUtilityEditor.GenerateEnumsFiles
             List<string> lines = new List<string>()
             {
                 "/* no change this file is generated*/",
+                "",
                 "namespace PlayFabUtility.AutoGeneration",
                 "{",
                 "    public enum Statistic",
@@ -45,7 +47,7 @@ namespace PlayFabUtilityEditor.GenerateEnumsFiles
             
             foreach (var playerStatisticDefinition in statisticDefinitions)
             {
-                lines.Add($"        {playerStatisticDefinition.StatisticName},");
+                lines.Add($"        {playerStatisticDefinition.StatisticName} = {playerStatisticDefinition.StatisticName.GetHashCode()},");
             }
             
             lines.Add("    }");
@@ -60,6 +62,7 @@ namespace PlayFabUtilityEditor.GenerateEnumsFiles
             List<string> lines = new List<string>()
             {
                 "/* no change this file is generated*/",
+                "",
                 "namespace PlayFabUtility.AutoGeneration",
                 "{",
                 "    public enum Currency",
@@ -68,7 +71,7 @@ namespace PlayFabUtilityEditor.GenerateEnumsFiles
             
             foreach (var currency in currencies)
             {
-                lines.Add($"        {currency.CurrencyCode},");
+                lines.Add($"        {currency.CurrencyCode} = {GetIntHashCode(currency.CurrencyCode)},");
             }
             
             lines.Add("    }");
@@ -108,5 +111,29 @@ namespace PlayFabUtilityEditor.GenerateEnumsFiles
                 "{\"name\": \""+name+"\"}",
             });
         }
+        
+        
+        static int GetIntHashCode(string strText)
+        {
+            int hashCode = 0;
+            if (!string.IsNullOrEmpty(strText))
+            {
+                //Unicode Encode Covering all characterset
+                byte[] byteContents = Encoding.Unicode.GetBytes(strText);
+                System.Security.Cryptography.SHA256 hash = 
+                    new System.Security.Cryptography.SHA256CryptoServiceProvider();
+                byte[] hashText = hash.ComputeHash(byteContents);
+                //32Byte hashText separate
+                //hashCodeStart = 0~7  8Byte
+                //hashCodeMedium = 8~23  8Byte
+                //hashCodeEnd = 24~31  8Byte
+                //and Fold
+                int hashCodeStart = BitConverter.ToInt32(hashText, 0);
+                int hashCodeMedium = BitConverter.ToInt32(hashText, 8);
+                int hashCodeEnd = BitConverter.ToInt32(hashText, 24);
+                hashCode = hashCodeStart ^ hashCodeMedium ^ hashCodeEnd;
+            }
+            return (hashCode);
+        }   
     }
 }
